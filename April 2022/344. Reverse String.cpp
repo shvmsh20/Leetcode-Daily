9d2477ab5216515b96685e31fa944356d088fc30
shvmsh20 <itsshivamsharma21@gmail.com>
2022-04-01 22:25:19 +0530

    1 April 2022

---
+++ April 2022/344. Reverse String.cpp
@@ -0,0 +1,8 @@
+void reverseString(vector<char>& s) {
+        int lo=0, hi=s.size()-1;
+        while(lo<hi){
+            swap(s[lo], s[hi]);
+            lo++;
+            hi--;
+        }
+    }
