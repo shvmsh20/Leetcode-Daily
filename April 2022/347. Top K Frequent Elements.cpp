b21030ea13ae0bf9ddeee2e7a5fdde7eb16fb17c
shvmsh20 <itsshivamsharma21@gmail.com>
2022-04-09 22:12:28 +0530

    8 April 2022

---
+++ April 2022/347. Top K Frequent Elements.cpp
@@ -0,0 +1,21 @@
+static bool cmp(pair<int, int> &a, pair<int, int> &b){
+        return a.second>b.second;
+    }
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        int n = nums.size();
+        for(int i=0; i<n; i++){
+            mp[nums[i]]++;
+        }
+        vector<pair<int, int>> v;
+        for(auto it: mp){
+            v.push_back(it);
+        }
+        sort(v.begin(), v.end(), cmp);
+        vector<int> res;
+        for(int i=0; i<k; i++){
+            res.push_back(v[i].first);
+        }
+        return res;
+        
+    }
