a348a05b618cf93e273fbb027aaf078fbc542b5c
shvmsh20 <itsshivamsharma21@gmail.com>
2022-04-17 10:54:55 +0530

    17 April 2022

---
+++ April 2022/Increasing Order Search Tree.cpp
@@ -0,0 +1,15 @@
+void dfs(TreeNode* root, TreeNode*& sentinel) {
+        if(!root) return;
+        dfs(root->left, sentinel);
+        TreeNode* newNode = new TreeNode(root->val);
+        sentinel->right = newNode;
+        sentinel = sentinel->right;
+        dfs(root->right, sentinel);
+        return;
+    }
+    TreeNode* increasingBST(TreeNode* root) {
+        TreeNode* sentinel = new TreeNode(0);
+        TreeNode* ans = sentinel;
+        dfs(root, sentinel);
+        return ans->right;
+    }
