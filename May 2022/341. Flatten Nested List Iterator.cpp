17748cdb2acedd183527c09c3a517bb4f83ff161
shvmsh20 <itsshivamsharma21@gmail.com>
2022-05-08 16:49:29 +0530

    8 May 2022

---
+++ May 2022/341. Flatten Nested List Iterator.cpp
@@ -0,0 +1,32 @@
+int curr=0;
+    vector<int> flattenList;
+    void helperFunc(vector<NestedInteger> &nestedList){
+        
+        int n=nestedList.size(),temp;        
+        vector<NestedInteger> temp2;
+        
+        for(int i=0;i<n;i++){
+            if(nestedList[i].isInteger()){
+                temp=nestedList[i].getInteger();
+                flattenList.push_back(temp);
+            }else{
+                temp2=nestedList[i].getList();
+                helperFunc(temp2);
+            }
+        } 
+        
+    }
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        helperFunc(nestedList);    
+    }
+    
+    int next() {
+        int ans=flattenList[curr];
+        curr++;
+        return ans;
+    }
+    
+    bool hasNext() {
+        if(curr==flattenList.size()) return false;    
+        return true;
+    }
