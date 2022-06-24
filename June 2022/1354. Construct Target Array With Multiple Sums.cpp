216471b122757fcb4e979da9845fabd6eff2ebd3
shvmsh20 <itsshivamsharma21@gmail.com>
2022-06-24 21:46:08 +0530

    24 June 2022

---
+++ June 2022/1354. Construct Target Array With Multiple Sums.cpp
@@ -0,0 +1,19 @@
+bool isPossible(vector<int>& t) {
+        priority_queue<long long>pq;
+        long long sum=0;
+        for(auto &i:t){
+            pq.push(i);
+            sum+=i;
+        }
+        while(pq.top()!=1){
+           sum -= pq.top();
+            if(sum == 0 || sum >= pq.top()) return false;
+            int old=pq.top()%sum;
+            sum+=old; 
+            if(old==0&&sum!=1 ) return false;
+            pq.pop();
+            pq.push(old);
+            
+        }
+        return true;
+    }
