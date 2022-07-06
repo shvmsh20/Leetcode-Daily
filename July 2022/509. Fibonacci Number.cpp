21e0c208a85d103cd6bc2992d3b0288eb3d68626
shvmsh20 <itsshivamsharma21@gmail.com>
2022-07-06 23:15:38 +0530

    6 July 2022

---
+++ July 2022/509. Fibonacci Number.cpp
@@ -0,0 +1,12 @@
+int fib(int n) {
+        int a=0, b=1;
+        if(n<=1){
+            return n;
+        }
+        for(int i=2; i<=n; i++){
+            int c = a+b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
