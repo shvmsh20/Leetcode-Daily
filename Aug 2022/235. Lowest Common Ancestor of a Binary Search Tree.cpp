7ea7453b10af55d39caa3278fa79fd0bfd81b58f
shvmsh20 <itsshivamsharma21@gmail.com>
2022-08-12 21:54:19 +0530

    12 Aug 2022

---
+++ Aug 2022/235. Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -0,0 +1,12 @@
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==nullptr){
+            return nullptr;
+        }
+        if(p->val<root->val && q->val<root->val){
+            return lowestCommonAncestor(root->left, p, q);
+        }else if(p->val>root->val && q->val>root->val){
+            return lowestCommonAncestor(root->right, p, q);
+        }else{
+            return root;
+        }
+    }
