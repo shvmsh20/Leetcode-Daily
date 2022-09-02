0d1e7c7b6c36abba01a730fb6a86c65135785955
shvmsh20 <itsshivamsharma21@gmail.com>
2022-09-02 19:45:59 +0530

    2 Sept 2022

---
+++ Sept 2022/637. Average of Levels in Binary Tree.cpp
@@ -0,0 +1,23 @@
+vector<double> averageOfLevels(TreeNode* root) {
+       vector<double> res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            double sum=0;
+            for(int i=0; i<n; i++){
+                TreeNode* t = q.front();
+                q.pop();
+                sum+= t->val;
+                if(t->left){
+                    q.push(t->left);
+                }
+                if(t->right){
+                    q.push(t->right);
+                }
+            }
+            double ans = sum/n;
+            res.push_back(ans);
+        }
+        return res;
+    }
