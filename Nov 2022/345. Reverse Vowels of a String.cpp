8f8ca66d27fa9dee11123ceac69bd680896d57b0
shvmsh20 <itsshivamsharma21@gmail.com>
2022-11-04 23:04:22 +0530

    4 Nov 2022

---
+++ Nov 2022/345. Reverse Vowels of a String.cpp
@@ -0,0 +1,14 @@
+string reverseVowels(string s) {
+        unordered_set<char> st = {'a','A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'};
+        int i=0, j=s.length()-1;
+        while(i<j){
+            while(!st.count(s[i]) && i<j){
+                i++;
+            }
+            while(!st.count(s[j]) && i<j) j--;
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+        return s;
+    }
