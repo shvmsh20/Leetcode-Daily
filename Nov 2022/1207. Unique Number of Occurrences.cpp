3261871482a3cbb8af3b05cd577f2985c2319ab2
shvmsh20 <itsshivamsharma21@gmail.com>
2022-11-30 22:27:25 +0530

    30 Nov 2022

---
+++ Nov 2022/1207. Unique Number of Occurrences.cpp
@@ -0,0 +1,20 @@
+bool uniqueOccurrences(vector<int>& arr) {
+        map<int,int> mp;                // map for storing each value occurance
+        
+        for(auto it : arr)
+        {
+            mp[it]++;                   // storing the occurance
+        }
+        
+        set<int> st;
+        
+        for(auto it :  mp)
+        {
+            // if occurance for any value is already present in the set then return false (not unique)
+            if(st.count(it.second)) return false;
+            
+            st.insert(it.second);                    // puting into the set (occurance)
+        }
+        
+        return true;   
+    }
