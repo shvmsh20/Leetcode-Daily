8dcbb34976d174385256e7d885cc4bb8a42e38bb
shvmsh20 <itsshivamsharma21@gmail.com>
2023-03-10 23:02:54 +0530

    10 March 2023

---
+++ March 2023/382. Linked List Random Node.cpp
@@ -0,0 +1,16 @@
+ListNode *head = NULL;
+    
+    Solution(ListNode* head) {
+        this->head = head;
+    }
+    
+    int getRandom() {
+        int ans = 0, i = 1;
+        ListNode *p = this->head;
+        while (p) {
+            if (rand() % i == 0) ans = p->val; // replace ans with i-th node.val with probability 1/i
+            i ++;
+            p = p->next;
+        }
+        return ans;
+    }
