7729db41d3469d0c48568b4b49701211d27cdd95
shvmsh20 <itsshivamsharma21@gmail.com>
2023-04-25 22:39:59 +0530

    25 April 2023

---
+++ April 2023/2336. Smallest Number in Infinite Set.cpp
@@ -0,0 +1,14 @@
+ map<int,int> mp;
+    SmallestInfiniteSet() {
+        for(int i =1;i<=1000;i++) mp[i]++;
+    }
+    
+    int popSmallest() {
+        int a = mp.begin()->first;
+        mp.erase(a);
+        return a;
+    }
+    
+    void addBack(int num) {
+        mp[num]++;
+    }
