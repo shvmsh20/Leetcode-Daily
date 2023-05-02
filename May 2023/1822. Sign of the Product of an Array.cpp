001f115e0f10044fdac96a44a7f39f64e8f53b68
shvmsh20 <itsshivamsharma21@gmail.com>
2023-05-02 22:07:13 +0530

    2 May 2023

---
+++ May 2023/1822. Sign of the Product of an Array.cpp
@@ -0,0 +1,11 @@
+int arraySign(vector<int>& nums) {
+        int pos=0,neg=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]>0) pos++;
+            else if(nums[i]<0) neg++;
+            else return 0;
+        }
+        if(neg%2==0) return 1;
+        return -1;
+    }
