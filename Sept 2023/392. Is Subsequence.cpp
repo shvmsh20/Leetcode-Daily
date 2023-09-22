da83e60bfaf12b06ea6af699195ce2dba1875004
shvmsh20 <itsshivamsharma21@gmail.com>
2023-09-22 21:11:07 +0530

    22 Sept 2023

---
+++ Sept 2023/392. Is Subsequence.cpp
@@ -0,0 +1,13 @@
+bool isSubsequence(string s, string t) {
+        int i=0, j=0;
+        int n = s.size(), m = t.size();
+        while(i<n && j<m){
+            if(s[i]==t[j]){
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        return i==n;
+    }
