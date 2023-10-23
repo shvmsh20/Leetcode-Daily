c617cd89366bb1c6cf05247cd194b7434199ba15
shvmsh20 <itsshivamsharma21@gmail.com>
2023-10-23 22:54:53 +0530

    23 Oct 2023

---
+++ Oct 2023/342. Power of Four.cpp
@@ -0,0 +1,17 @@
+bool isPowerOfFour(int n) {
+        int cpy = n;
+        if(n==0 || n<0){
+            return false;
+        }
+        int count=0;
+        while(n!=1){
+            count++;
+            n = n>>1;
+        }
+        
+        if((count%2==0) && ((1<<count)==cpy)){
+            return true;
+        }else{
+            return false;
+        }
+    }
