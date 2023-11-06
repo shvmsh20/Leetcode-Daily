f896c17a9e22085463803623c61d6d6c5476c21a
shvmsh20 <itsshivamsharma21@gmail.com>
2023-11-06 21:39:16 +0530

    06 Nov 2023

---
+++ Nov 2023/1845. Seat Reservation Manager.cpp
@@ -0,0 +1,15 @@
+priority_queue <int, vector<int>, greater<int> > pq; 
+public:
+    SeatManager(int n) {
+        for(int i=1;i<=n;i++)
+            pq.push(i);
+    }
+    int reserve() {
+        int x=pq.top();
+        pq.pop();
+        return x;
+    }
+    
+    void unreserve(int seatNumber) {
+        pq.push(seatNumber);
+    }
