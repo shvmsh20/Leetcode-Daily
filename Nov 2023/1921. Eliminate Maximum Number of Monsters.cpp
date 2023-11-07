09d6fc5adab43bcc6d6d475608093bf6210c9dbf
shvmsh20 <itsshivamsharma21@gmail.com>
2023-11-07 23:51:42 +0530

    07 Nov 2023

---
+++ Nov 2023/1921. Eliminate Maximum Number of Monsters.cpp
@@ -0,0 +1,18 @@
+int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        int n = dist.size();
+        std::vector<double> time_to_city(n);
+        
+        for (int i = 0; i < n; ++i) {
+            time_to_city[i] = static_cast<double>(dist[i]) / speed[i];
+        }
+        
+        std::sort(time_to_city.begin(), time_to_city.end());
+        
+        for (int i = 0; i < n; ++i) {
+            if (time_to_city[i] <= i) {
+                return i;
+            }
+        }
+        
+        return n;
+    }
