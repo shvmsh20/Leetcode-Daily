805345cc55bec5ecd1e84e18a4e7ae6b72e05c9a
shvmsh20 <itsshivamsharma21@gmail.com>
2023-11-16 22:49:00 +0530

    16 Nov 2023

---
+++ Nov 2023/1980. Find Unique Binary String.cpp
@@ -0,0 +1,9 @@
+string findDifferentBinaryString(vector<string>& nums) {
+         string result;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            result += (nums[i][i] == '0' ? '1' : '0');
+        }
+
+        return result;
+    }
