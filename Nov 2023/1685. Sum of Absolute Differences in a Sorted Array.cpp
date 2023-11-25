2570f12b4bc5a9101e73cbeefafd6e48be9514b2
shvmsh20 <itsshivamsharma21@gmail.com>
2023-11-25 15:35:42 +0530

    25 Nov 2023

---
+++ Nov 2023/1685. Sum of Absolute Differences in a Sorted Array.cpp
@@ -0,0 +1,13 @@
+vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+        int n=nums.size();
+        nums.push_back(0);//Adding 0 for avoid of if-branch in the loop
+        int A=0, B=accumulate(nums.begin()+1, nums.end(), 0);
+        vector<int> ans(n, 0);
+        #pragma unroll
+        for(int i=0; i<n; i++){
+            ans[i]=(2*i-n+1)*nums[i]+B-A;
+            A+=nums[i];
+            B-=nums[i+1];
+        }
+        return ans;
+    }
