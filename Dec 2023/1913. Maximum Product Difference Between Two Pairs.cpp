2594c153508378a03422b372e97a3a14a88a28d0
shvmsh20 <itsshivamsharma21@gmail.com>
2023-12-18 22:39:49 +0530

    18 Dec 2023

---
+++ Dec 2023/1913. Maximum Product Difference Between Two Pairs.cpp
@@ -0,0 +1,5 @@
+int maxProductDifference(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return (nums[n-1]*nums[n-2]) - (nums[0]*nums[1]);
+    }
