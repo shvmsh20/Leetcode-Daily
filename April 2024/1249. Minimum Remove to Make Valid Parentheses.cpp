aca83d0fb58ac795d5e0ae1c44c0c11e275eedb2
shvmsh20 <itsshivamsharma21@gmail.com>
2024-04-06 09:33:15 +0530

    06 April 2024

---
+++ April 2024/1249. Minimum Remove to Make Valid Parentheses.cpp
@@ -0,0 +1,21 @@
+string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        int n = s.size();
+        for(int i=0; i<n; i++){
+            if(s[i]=='('){
+                st.push(i);
+            }else if(s[i]==')'){
+                if(st.empty()){
+                    s[i]='@';
+                }else{
+                    st.pop();
+                }
+            }
+        }
+        while(!st.empty()){
+            s[st.top()]='@';
+            st.pop();
+        }
+        s.erase(remove(s.begin(), s.end(), '@'), s.end());
+       return s;
+    }
