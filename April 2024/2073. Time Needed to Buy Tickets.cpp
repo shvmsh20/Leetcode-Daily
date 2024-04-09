7789fc0bfe882bdbd464a465b7a25e3323e2d5d8
shvmsh20 <itsshivamsharma21@gmail.com>
2024-04-09 23:13:09 +0530

    09 April 2024

---
+++ April 2024/2073. Time Needed to Buy Tickets.cpp
@@ -0,0 +1,10 @@
+int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int n=tickets.size();
+        int x=tickets[k];
+        int time=0;
+        for(int i=0; i<n; i++){
+            int buy=(i>k)?x-1:x;
+            time+=min(buy, tickets[i]);
+        }
+        return time;   
+    }
