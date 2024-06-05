7e159ecc36bb1bfae374a7ca7df1870aa5285cce
shvmsh20 <itsshivamsharma21@gmail.com>
2024-06-05 23:12:02 +0530

    05 June 2024

---
+++ June 2024/1002. Find Common Characters.cpp
@@ -0,0 +1,15 @@
+vector<int> count(const string& str) {
+        vector<int> frequency(26, 0);
+        for (char c : str) {
+            frequency[c - 'a']++;
+        }
+        return frequency;
+    }
+    
+    vector<int> intersection(const vector<int>& a, const vector<int>& b) {
+        vector<int> t(26, 0);
+        for (int i = 0; i < 26; i++) {
+            t[i] = min(a[i], b[i]);
+        }
+        return t;
+    }
