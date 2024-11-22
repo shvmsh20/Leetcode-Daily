d07d8cf597b91150d7501f59f5811f30e56afecd
shvmsh20 <itsshivamsharma21@gmail.com>
2024-11-22 22:52:30 +0530

    22 Nov 2024

---
+++ Nov 2024/1072. Flip Columns For Maximum Number of Equal Rows.cpp
@@ -0,0 +1,20 @@
+int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+        int n = matrix.size();  
+        int m = matrix[0].size(); 
+        unordered_map<string, int> mp;
+        
+        for (const auto& row : matrix) {
+            string temp = "";
+            for (int j = 0; j < m; ++j) {
+                temp += (row[j] == row[0] ? '1' : '0');
+            }
+            mp[temp]++;
+        }
+        
+        int maxi = 0;
+        for (auto it : mp) {
+            maxi = max(maxi, it.second);
+        }
+        
+        return maxi;
+    }
