4c15f13acf3409661bc9d70c47ad5886fac39735
shvmsh20 <itsshivamsharma21@gmail.com>
2024-12-06 22:51:13 +0530

    06 Dec 2024

---
+++ Dec 2024/2554. Maximum Number of Integers to Choose From a Range I.cpp
@@ -0,0 +1,13 @@
+int maxCount(vector<int>& banned, int n, int maxSum) {
+        bitset<10001> xNot=0;
+        for(int x: banned)
+            xNot[x]=1;
+        int sum=0, cnt=0;
+        for(int x=1; x<=n && sum<=maxSum; x++){
+            if (!xNot[x]){
+                sum+=x;
+                cnt++;
+            }
+        }
+        return (sum<=maxSum)?cnt:cnt-1;
+    }
