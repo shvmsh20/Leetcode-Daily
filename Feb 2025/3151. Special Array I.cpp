cc981e1e9f964946356021dd635a64a8690118b9
shvmsh20 <itsshivamsharma21@gmail.com>
2025-02-01 22:31:01 +0530

    01 Feb 2025

---
+++ Feb 2025/3151. Special Array I.cpp
@@ -0,0 +1,6 @@
+bool isArraySpecial(vector<int>& nums) {
+        for (int i = 1; i < nums.size(); ++i)
+            if (!((nums[i] ^ nums[i-1]) & 1)) 
+                return false;
+        return true;
+    }
