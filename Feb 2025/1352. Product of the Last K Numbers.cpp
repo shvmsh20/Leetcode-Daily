4e9ad6068ba0eadd044f63c4cc0d556f9111b51b
shvmsh20 <itsshivamsharma21@gmail.com>
2025-02-14 23:13:20 +0530

    14 Feb 2025

---
+++ Feb 2025/1352. Product of the Last K Numbers.cpp
@@ -0,0 +1,17 @@
+vector<int> prefixProduct;
+    ProductOfNumbers() {
+        prefixProduct.push_back(1);
+    }
+    void add(int num) {
+        if (num == 0) {
+            prefixProduct.clear();
+            prefixProduct.push_back(1);
+        } else {
+            prefixProduct.push_back(num * prefixProduct.back());
+        }
+    }
+    int getProduct(int k) {
+        int size = prefixProduct.size();
+        if (k >= size) return 0;
+        return prefixProduct.back() / prefixProduct[size - k - 1];
+    }
