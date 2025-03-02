db5dec1f41afdf605522266d495d883fd382da5e
shvmsh20 <itsshivamsharma21@gmail.com>
2025-03-02 13:37:20 +0530

    02 March 2025

---
+++ March 2025/2570. Merge Two 2D Arrays by Summing Values.cpp
@@ -0,0 +1,27 @@
+vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        vector<vector<int>> res;
+        int n = nums1.size(), m = nums2.size();
+        int i=0, j=0;
+        while(i<n && j<m){
+            if(nums1[i][0]<nums2[j][0]){
+                res.push_back(nums1[i]);
+                i++;
+            }else if(nums1[i][0]>nums2[j][0]){
+                res.push_back(nums2[j]);
+                j++;
+            }else{
+                res.push_back({nums1[i][0], nums1[i][1]+nums2[j][1]});
+                i++;
+                j++;
+            }
+        }
+        while(i<n){
+            res.push_back(nums1[i]);
+            i++;
+        }
+        while(j<m){
+            res.push_back(nums2[j]);
+            j++;
+        }
+        return res;
+    }
