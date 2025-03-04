f42e5d76cf196c54e8fa893640f659e2b9b3c9f8
shvmsh20 <itsshivamsharma21@gmail.com>
2025-03-04 22:40:25 +0530

    04 March 2025

---
+++ March 2025/1780. Check if Number is a Sum of Powers of Three.cpp
@@ -0,0 +1,9 @@
+bool checkPowersOfThree(int n) {
+         while (n > 0) {
+            if (n % 3 == 2) {
+                return false;
+            }
+            n /= 3;
+        }
+        return true;
+    }
