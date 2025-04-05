f355b49ecf7d986b9844ad8f6e0e8ffaa3fc2377
shvmsh20 <itsshivamsharma21@gmail.com>
2025-04-05 10:44:27 +0530

    05 April 2025

---
+++ April 2025/1863. Sum of All Subset XOR Totals.cpp
@@ -0,0 +1,16 @@
+int result;
+    void helper(vector<int>& nums,int sum,int i)
+    {
+        if(i==nums.size()-1) {
+            result+= sum + (sum^nums[i]);
+            return;
+        }
+        helper(nums,sum,i+1);
+        helper(nums,sum^nums[i],i+1);
+    }
+public:
+    int subsetXORSum(vector<int>& nums) {
+        result=0;
+        helper(nums,0,0);
+        return result;
+    }
