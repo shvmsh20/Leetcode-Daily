a53ba19bffefda028f7bca6b63edcd7123964593
shvmsh20 <itsshivamsharma21@gmail.com>
2025-04-17 23:27:39 +0530

    17 April 2025

---
+++ April 2025/2176. Count Equal and Divisible Pairs in an Array.cpp
@@ -0,0 +1,11 @@
+int countPairs(vector<int>& nums, int k) {
+        int n = nums.size(), res = 0;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(nums[i]==nums[j] && i*j%k==0){
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
