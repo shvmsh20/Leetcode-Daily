10d4de06bedc40cd876ff2f5763e76c52530837d
shvmsh20 <itsshivamsharma21@gmail.com>
2025-08-29 22:40:14 +0530

    29 Aug 2025

---
+++ Aug 2025/3021. Alice and Bob Playing Flower Game.cpp
@@ -0,0 +1,7 @@
+long long flowerGame(int n, int m) {
+        int odda=(n+1)/2;
+       int evena=n/2;
+       int oddb=(m+1)/2;
+       int evenb=m/2;
+       return (long long)odda * evenb + (long long)evena * oddb;
+    }
