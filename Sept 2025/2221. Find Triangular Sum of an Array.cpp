aa018f126b2bada24727b067e40c6157440828f4
shvmsh20 <itsshivamsharma21@gmail.com>
2025-09-30 22:09:39 +0530

    30 Sept 2025

---
+++ Sept 2025/2221. Find Triangular Sum of an Array.cpp
@@ -0,0 +1,10 @@
+int triangularSum(vector<int>& nums) {
+        int n = nums.size();
+        while(n>1){
+            for(int i=0; i<n-1; i++){
+                nums[i] = (nums[i]+nums[i+1])%10;
+            }
+            n--;
+        }
+        return nums[0];
+    }
