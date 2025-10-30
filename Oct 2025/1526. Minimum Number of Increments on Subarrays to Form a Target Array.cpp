e2cf13b77ec847c4ac361bcb273849cbffd475d7
shvmsh20 <itsshivamsharma21@gmail.com>
2025-10-30 23:24:31 +0530

    30 Oct 2025

---
+++ Oct 2025/1526. Minimum Number of Increments on Subarrays to Form a Target Array.cpp
@@ -0,0 +1,6 @@
+int minNumberOperations(vector<int>& target) {
+         int count = target[0];
+        for (int i = 1; i < target.size(); i++)
+            count += max(target[i] - target[i - 1], 0);
+        return count;
+    }
