ea2234e78091dbf8d8b2fac6538ccf9f5f98d2c4
shvmsh20 <itsshivamsharma21@gmail.com>
2025-11-18 22:49:37 +0530

    18 Nov 2025

---
+++ Nov 2025/717. 1-bit and 2-bit Characters.cpp
@@ -0,0 +1,8 @@
+ bool isOneBitCharacter(vector<int>& bits) {
+         int n = bits.size();
+        int i = 0;        
+        while (i < n - 1)
+            i += bits[i] + 1;
+
+        return i == n - 1;
+    }
