40e514d02a1f562948be58751ff0299418920ff1
shvmsh20 <itsshivamsharma21@gmail.com>
2025-12-22 22:54:55 +0530

    22 Dec 2025

---
+++ Dec 2025/960. Delete Columns to Make Sorted III.cpp
@@ -0,0 +1,26 @@
+int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].size();
+
+        vector<int> dp(m, 1);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < i; j++) {
+                bool valid = true;
+
+                for (int r = 0; r < n; r++) {
+                    if (strs[r][j] > strs[r][i]) {
+                        valid = false;
+                        break;
+                    }
+                }
+
+                if (valid) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int keep = *max_element(dp.begin(), dp.end());
+        return m - keep;
+    }
