99dd267414af0c7accad1918c3f04dcec2f22a30
shvmsh20 <itsshivamsharma21@gmail.com>
2026-03-29 22:13:30 +0530

    29 March 2026

---
+++ March 2026/2839. Check if Strings Can be Made Equal With Operations I.cpp
@@ -0,0 +1,27 @@
+bool canBeEqual(string s1, string s2) {
+        if(s1==s2){
+            return true;
+        }
+        swap(s1[0], s1[2]);
+         if(s1==s2){
+            return true;
+        }
+         if(s1==s2){
+            return true;
+        }
+        swap(s1[1], s1[3]);
+         if(s1==s2){
+            return true;
+        }
+         if(s1==s2){
+            return true;
+        }
+        swap(s1[0], s1[2]);
+         if(s1==s2){
+            return true;
+        }
+         if(s1==s2){
+            return true;
+        }
+        return false;
+    }
