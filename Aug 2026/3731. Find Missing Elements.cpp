598db0f11027b554c8a89e2abc5a2fa9a14c1ac5
shvmsh20 <itsshivamsharma21@gmail.com>
2026-08-04 22:36:49 +0530

    04 Aug 2026

---
+++ Aug 2026/3731. Find Missing Elements.cpp
@@ -0,0 +1,16 @@
+vector<int> findMissingElements(vector<int>& nums) {
+         unordered_set<int> seen(nums.begin(), nums.end());
+
+        int mn = *min_element(nums.begin(), nums.end());
+        int mx = *max_element(nums.begin(), nums.end());
+
+        vector<int> ans;
+
+        for (int x = mn; x <= mx; x++) {
+            if (!seen.count(x)) {
+                ans.push_back(x);
+            }
+        }
+
+        return ans;
+    }
