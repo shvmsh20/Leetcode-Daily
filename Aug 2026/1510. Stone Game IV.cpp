3006975f08a7332111a90b028348d1583de2ddd5
shvmsh20 <itsshivamsharma21@gmail.com>
2026-08-10 23:23:59 +0530

    10 Aug 2026

---
+++ Aug 2026/1510. Stone Game IV.cpp
@@ -0,0 +1,12 @@
+bool winnerSquareGame(int n) {
+        vector<bool>dp(n+1,false);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j*j<=i;j++){
+            if(!dp[i-j*j]){
+                dp[i]=true;
+                break;
+            }
+        }
+    }
+    return dp[n];
+    }
